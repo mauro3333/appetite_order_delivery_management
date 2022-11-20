04b4154c5d4d275611e14231a8e9dc2c8fd0e89a
diego <mauro3333@gmail.com>
2022-11-19 16:34:39 -0800

    initial commit

---
+++ src/main/java/com/appetite/appetite_order_delivery_management/AppetiteOrderDeliveryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.appetite.appetite_order_delivery_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppetiteOrderDeliveryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppetiteOrderDeliveryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appetite/appetite_order_delivery_management/AppetiteOrderDeliveryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appetite.appetite_order_delivery_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppetiteOrderDeliveryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
