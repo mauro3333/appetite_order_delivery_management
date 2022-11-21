d1b2bc20e710427fb677a47fd9bfe3244551fd6a
chicacode <gerahernan.al@gmail.com>
2022-11-20 21:24:56 -0800

    feat: added Cliente entity

---
+++ src/main/java/com/appetite/appetite_order_delivery_management/entity/Client.java
@@ -0,0 +1,93 @@
+package com.appetite.appetite_order_delivery_management.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long clientId;
+
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String paymentMethod;
+    private String address;
+
+    public Client() {
+
+    }
+
+    public Client(Long clientId, String firstName, String lastName, String phone, String email, String paymentMethod,
+            String address) {
+        this.clientId = clientId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.paymentMethod = paymentMethod;
+        this.address = address;
+    }
+
+    public Long getClientId() {
+        return this.clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
