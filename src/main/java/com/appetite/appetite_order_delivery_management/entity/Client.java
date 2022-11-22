d6311a75ca4694c50438fe7596e20147d94ee0be
diego <mauro3333@gmail.com>
2022-11-21 18:49:27 -0800

    changes

--- src/main/java/com/appetite/appetite_order_delivery_management/entity/Client.java
+++ src/main/java/com/appetite/appetite_order_delivery_management/entity/Client.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-<<<<<<< HEAD
 
 @Entity
 public class Client {
@@ -79,91 +78,5 @@ public class Client {
 		this.methodOfPayment = methodOfPayment;
 	}
 
-=======
-@Entity
-public class Client {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String firstName;
-    private String lastName;
-    private String phone;
-    private String email;
-    private String paymentMethod;
-    private String address;
-
-    public Client() {
-
-    }
-
-    public Client(Long clientId, String firstName, String lastName, String phone, String email, String paymentMethod,
-            String address) {
-        this.clientId = clientId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.email = email;
-        this.paymentMethod = paymentMethod;
-        this.address = address;
-    }
-
-    public Long getClientId() {
-        return this.clientId;
-    }
-
-    public void setClientId(Long clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPaymentMethod() {
-        return this.paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
->>>>>>> 28ae96c5778bf51f8e7c979ece45ce6ee569fd2f
 
 }
--- src/main/java/com/appetite/appetite_order_delivery_management/entity/Food.java
+++ src/main/java/com/appetite/appetite_order_delivery_management/entity/Food.java
@@ -1,14 +1,10 @@
 package com.appetite.appetite_order_delivery_management.entity;
 
-<<<<<<< HEAD
 import javax.persistence.Entity;
-=======
->>>>>>> 28ae96c5778bf51f8e7c979ece45ce6ee569fd2f
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-<<<<<<< HEAD
 
 @Entity
 public class Food {
@@ -31,23 +27,10 @@ public class Food {
         this.price = price;
         this.isAvailable = isAvailable;
     }
-=======
-public class Food {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String name;
-    private String ingredients;
-    private Double calories;
-    private Double price;
-    private Boolean isAvailable;
->>>>>>> 28ae96c5778bf51f8e7c979ece45ce6ee569fd2f
 
     public Food() {
     }
 
-<<<<<<< HEAD
     public Long getFoodId() {
         return foodId;
     }
@@ -87,67 +70,5 @@ public class Food {
     
 
 
-=======
-    public Food(Long id, String name, String ingredients, Double calories, Double price, Boolean isAvailable) {
-        this.id = id;
-        this.name = name;
-        this.ingredients = ingredients;
-        this.calories = calories;
-        this.price = price;
-        this.isAvailable = isAvailable;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIngredients() {
-        return this.ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Double getCalories() {
-        return this.calories;
-    }
-
-    public void setCalories(Double calories) {
-        this.calories = calories;
-    }
-
-    public Double getPrice() {
-        return this.price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Boolean isIsAvailable() {
-        return this.isAvailable;
-    }
-
-    public Boolean getIsAvailable() {
-        return this.isAvailable;
-    }
-
-    public void setIsAvailable(Boolean isAvailable) {
-        this.isAvailable = isAvailable;
-    }
->>>>>>> 28ae96c5778bf51f8e7c979ece45ce6ee569fd2f
 
 }
